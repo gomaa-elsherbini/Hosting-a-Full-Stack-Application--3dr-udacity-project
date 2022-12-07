aae9c483047aba2afba051aa20b929fc305374a2
gomaa <gogochemist35@gmail.com>
2022-12-07 21:57:29 +0200

    inatial commit

---
+++ udagram/set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=postgres
+export POSTGRES_HOST=mydbinstance.csxbuclmtj3c.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=udagram_database
+export AWS_BUCKET=arn:aws:s3:::mygogobucket
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=my-secret-jwtsecret
+export URL=http://localhost:8010
