f094acba35d80a0d73db3f4c70d716beda00c172
gomaa <gogochemist35@gmail.com>
2022-12-08 01:49:07 +0200

    modify udagram/set_env.sh

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -6,6 +6,6 @@ export POSTGRES_HOST=mydbinstance.csxbuclmtj3c.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=udagram_database
 export AWS_BUCKET=arn:aws:s3:::mygogobucket
 export AWS_REGION=us-east-1
-export AWS_PROFILE=default
+export AWS_PROFILE=eb-cli
 export JWT_SECRET=my-secret-jwtsecret
 export URL=http://localhost:8010
