5e1a0ed01bf5490ad30d19bc3e23b40806f33494
gomaa <gogochemist35@gmail.com>
2022-12-08 16:52:36 +0200

    edit some issus

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -2,7 +2,7 @@
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
 export POSTGRES_PASSWORD=postgres
-export POSTGRES_HOST=mydbinstance.csxbuclmtj3c.us-east-1.rds.amazonaws.com
+export POSTGRES_HOST=database-1.crakqfejqysg.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=udagram_database
 export AWS_BUCKET=arn:aws:s3:::mygogobucket
 export AWS_REGION=us-east-1
--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: 'Udagram',
-  apiHost: 'http://udagram-api-dev2222222.us-east-1.elasticbeanstalk.com/api/v0',
+  apiHost: 'http://udagram-api-env.eba-phwjqsrw.us-east-1.elasticbeanstalk.com/api/v0',
 };
 
 /*
