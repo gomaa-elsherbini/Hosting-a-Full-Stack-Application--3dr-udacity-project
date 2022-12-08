8e3707be388e66297f0a977798d3114e74598cd2
gomaa <gogochemist35@gmail.com>
2022-12-09 00:50:27 +0200

    fixing some issuse

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -5,14 +5,14 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://udagram-api-env.eba-phwjqsrw.us-east-1.elasticbeanstalk.com/api/v0',
+  apiHost: 'http://udagram-api-env.eba-phwjqsrw.us-east-1.elasticbeanstalk.com',
 };
 
 /*
 * For easier debugging in development mode, you can import the following file
 * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
-*
 import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+*
  * This import should be commented out in production mode because it will have a negative impact
  * on performance if an error is thrown.
  */
