d771b365bee233cc0fa420b690b30069b7de2d5d
gomaa <gogochemist35@gmail.com>
2022-12-09 04:31:29 +0200

    new edit

--- udagram/udagram-api/src/server.ts
+++ udagram/udagram-api/src/server.ts
@@ -26,7 +26,7 @@ import { V0_FEED_MODELS, V0_USER_MODELS } from "./controllers/v0/model.index";
 
   const app = express();
   
-  const port = process.env.PORT || 8080;
+  const port = process.env.PORT || process.env.DB_PORT;
 
   app.use(bodyParser.json());
 
