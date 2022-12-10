49df4f3d50610a9f03168f7c5b526fe09023f640
gomaa <gogochemist35@gmail.com>
2022-12-10 20:02:42 +0200

    update aws user credintials

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
+  production: false,
   appName: 'Udagram',
   apiHost: 'http://udagram-api-env.eba-ch52xm39.us-east-1.elasticbeanstalk.com/api/v0',
 };
